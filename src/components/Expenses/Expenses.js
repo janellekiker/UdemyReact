1fb978878a57470d128991e300ca40ed8c57fa0c
Janelle Kiker <janellekiker@gmail.com>
2022-12-01 14:37:13 -0600

    finished code

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 import ExpenseItem from "./ExpenseItem";
 
 function Expenses(props) {
@@ -37,6 +38,7 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
