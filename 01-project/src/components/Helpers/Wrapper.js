681def8aba8a3cf08df1969c68faf3f16d8d67f5
Janelle Kiker <113371576+janellekiker@users.noreply.github.com>
2022-12-05 15:46:33 -0600

    Add files via upload

---
+++ 01-project/src/components/Helpers/Wrapper.js
@@ -0,0 +1,6 @@
+function Wrapper(props) {
+  return props.children;
+  // childre props holds all the content you're passing in the opening and closing component of your custom component
+}
+
+export default Wrapper;
