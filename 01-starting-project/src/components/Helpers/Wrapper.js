baa27f267e424c5d875cd5ec6741f25853c7dc72
Janelle Kiker <janellekiker@gmail.com>
2022-12-07 09:09:27 -0600

    refs

---
+++ 01-starting-project/src/components/Helpers/Wrapper.js
@@ -0,0 +1,6 @@
+function Wrapper(props) {
+  return props.children;
+  // children props holds all the content you're passing in the opening and closing component of your custom component
+}
+
+export default Wrapper;
